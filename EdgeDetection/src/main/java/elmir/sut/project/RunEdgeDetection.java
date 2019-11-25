19c669d3406d6575cee22b9b0fd8b8df4db540e6
Elmir Sut <elmirsutisz@gmail.com>
2019-11-25 08:57:48 +0100

    Import of current sequential version of the project

---
+++ EdgeDetection/src/main/java/elmir/sut/project/RunEdgeDetection.java
@@ -0,0 +1,13 @@
+package elmir.sut.project;
+
+import java.io.IOException;
+
+import elmir.sut.project.ui.EdgeDetectionUI;
+
+
+public class RunEdgeDetection {
+
+    public static void main(String[] args) throws IOException {
+        new EdgeDetectionUI();
+    }
+}
